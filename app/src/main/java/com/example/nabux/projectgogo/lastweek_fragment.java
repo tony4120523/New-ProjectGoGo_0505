5f0a223bb30944fd1b63e990071f5ea2999af2b5
DragonH <a0261038@g2.usc.edu.tw>
2016-08-22 23:24:38 +0800

    show compared result in listfragment

--- app/src/main/java/com/example/nabux/projectgogo/lastweek_fragment.java
+++ app/src/main/java/com/example/nabux/projectgogo/lastweek_fragment.java
@@ -22,11 +22,12 @@ import java.util.HashMap;
 public class lastweek_fragment extends Fragment{
     private SimpleAdapter adapter;
     ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
-    double[] thisweek_ans = new double[]{26.17,12796.0,1559.71,1537.86};
-    String[] thisweek_item=new String[]{"本週平均bmi為："+thisweek_ans[0],"本週平均步數為："+thisweek_ans[1],"本週平均心肌收縮壓為："+thisweek_ans[2],"本週平均平均心肌舒張為："+thisweek_ans[3],};
-
-    double[] lastweek_ans = new double[]{28.17,22796.0,1759.71,1737.86};
-    String[] compare=new String[]{"比上週平均bmi少了："+(lastweek_ans[0]-thisweek_ans[0]),"比上週平均步數少了："+(lastweek_ans[1]-thisweek_ans[1]),"比上週平均心肌收縮壓少了："+(lastweek_ans[2]-thisweek_ans[2]),"比上週平均平均心肌舒張少了："+(lastweek_ans[3]-thisweek_ans[3]),};
+    double[] lastweek_ans = new double[]{36.0,12796.0,143.0,88.0};
+    String[] lastweek_item=new String[]{"本週平均bmi為："+lastweek_ans[0],"本週平均步數為："+lastweek_ans[1],"本週平均心肌收縮壓為："+lastweek_ans[2],"本週平均平均心肌舒張壓為："+lastweek_ans[3],};
+    //double[] lastweek_ans = new double[]{28.17,22796.0,144.0,77.0};
+    //String[] compare=new String[]{"比上週平均bmi少了："+(lastweek_ans[0]-thisweek_ans[0]),"比上週平均步數少了："+(lastweek_ans[1]-thisweek_ans[1]),"比上週平均心肌收縮壓少了："+(lastweek_ans[2]-thisweek_ans[2]),"比上週平均平均心肌舒張少了："+(lastweek_ans[3]-thisweek_ans[3]),};
+    String[]compare=new String[]{"","","",""};
+    String bmians,stepans,sysbpans,diabpans,bsans;
     ListView list1;
 
     public static lastweek_fragment newInstance() {
@@ -43,10 +44,13 @@ public class lastweek_fragment extends Fragment{
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.lastweek_show, container, false);
         list1= (ListView) rootView.findViewById(R.id.list1);
-
-        for(int i=0; i<thisweek_item.length; i++){
+        count_bmi();
+        count_step();
+        count_sysbp();
+        count_diabp();
+        for(int i=0; i<lastweek_item.length; i++){
             HashMap<String,String> item = new HashMap<String,String>();
-            item.put( "thisweek", thisweek_item[i]);
+            item.put( "thisweek", lastweek_item[i]);
             item.put( "compared",compare[i] );
             list.add( item );
         }
@@ -61,15 +65,81 @@ public class lastweek_fragment extends Fragment{
         list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(), "顯示"+thisweek_item[position], Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "顯示"+lastweek_item[position], Toast.LENGTH_SHORT).show();
             }
         });
         return rootView;
     }
 
+public String count_bmi(){
+    if(lastweek_ans[0]<18.5){
+        bmians="您的體位為 過輕";
+        compare[0]=bmians;
+
+    }
+    else if(lastweek_ans[0]>=18.5 && lastweek_ans[0]<24.0){
+        bmians="您的體位為 正常";
+        compare[0]=bmians;
+
+    }
+    else if(lastweek_ans[0]>=24.0 && lastweek_ans[0]<27.0){
+        bmians="您的體位為 過重";
+        compare[0]=bmians;
+
+    }
+    else if(lastweek_ans[0]>=27.0 && lastweek_ans[0]<30.0){
+        bmians="您的體位為 輕度肥胖";
+        compare[0]=bmians;
+    }
+    else if(lastweek_ans[0]>=30.0 && lastweek_ans[0]<35.0){
+        bmians="您的體位為 中度肥胖";
+        compare[0]=bmians;
+    }
+    else if(lastweek_ans[0]>=35.0){
+        bmians="您的體位為 重度肥胖";
+        compare[0]=bmians;
+    }
+
+    return bmians;
+}
+    public String count_step(){
+        if(lastweek_ans[1]<6000.0){
+            stepans="距離建議步數還有 "+(6000-lastweek_ans[1])+" 步";
+            compare[1]=stepans;
+        } else if (lastweek_ans[1]>6000.0) {
+            stepans="太棒了!! 比建議步數多了 "+(lastweek_ans[1]-6000)+" 步";
+            compare[1]=stepans;
+        }
+        return stepans;
+    }
+
+    public String count_sysbp(){
+        if(lastweek_ans[2]>=111.0 &&lastweek_ans[2]<=143.0){
+            sysbpans="心肌收縮壓正常!! 正常值為 127 + - 16 (即111~143)";
+            compare[2]=sysbpans;
+        }
+        else{
+            sysbpans="心肌收縮壓異常!! 正常值為 127 + - 16 (即111~143)";
+            compare[2]=sysbpans;
+        }
 
 
+        return sysbpans;
+    }
 
+    public String count_diabp(){
+        if(lastweek_ans[3]>=67.0 &&lastweek_ans[2]<=87.0){
+            sysbpans="心肌舒張壓正常!! 正常值為 77 + - 10 (即67~87)";
+            compare[3]=sysbpans;
+        }
+        else{
+            sysbpans="心肌舒張壓異常!! 正常值為 77 + - 10 (即67~87)";
+            compare[3]=sysbpans;
+        }
+
+
+        return diabpans;
+    }
 
 
 }
